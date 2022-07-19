c60d8555ade437b3409b94f44b3b395b8f3a1d1a
ChangHyunY <49017188+ChangHyunY@users.noreply.github.com>
2022-07-19 15:03:08 +0900

    SafeArea 수정
    Default 값으로 변경

--- SafeArea.cs
+++ SafeArea.cs
@@ -1,36 +1,23 @@
 ﻿using UnityEngine;
 
-namespace Mobile
+namespace Unility
 {
-    /// <summary>
-    /// 모바일 기기에 따른 SafeArea 설정
-    /// </summary>
-    /// <사용법>
-    /// 1. 캔버스 하단에 빈 오브젝트 생성 후 Full Stretch 설정 (ALT + 5시 방향)
-    /// 2. 해당 클래스를 Add 후 FixedPosition의 크기만큼 하단에서 줄어듬
-    public class SafeArea : MonoBehaviour
+    namespace UI
     {
-        private RectTransform rectTransform;
-        private Rect safeArea;
-        private Vector2 minAnchor;
-        private Vector2 maxAnchor;
-
-        public Vector2 fixedPosition = new Vector2(0, 150f);
-
-        private void Awake()
+        public class SafeArea : MonoBehaviour
         {
-            rectTransform = GetComponent<RectTransform>();
-            safeArea = Screen.safeArea;
+            private RectTransform rectTransform;
+            private Rect safeArea;
+            private Vector2 minAnchor;
+            private Vector2 maxAnchor;
 
-            if(!EqualRectSize(rectTransform, safeArea))
+            private void Awake()
             {
-                //위젯이 활성화 되어있지 않을 때
-                //minAnchor = safeArea.position;
-                //maxAnchor = minAnchor + safeArea.size;
+                rectTransform = GetComponent<RectTransform>();
+                safeArea = Screen.safeArea;
 
-                //위젯이 활성화 되어 있을 때
-                minAnchor = safeArea.position + fixedPosition;
-                maxAnchor = safeArea.size;
+                minAnchor = safeArea.position;
+                maxAnchor = minAnchor + safeArea.size;
 
                 minAnchor.x /= Screen.width;
                 minAnchor.y /= Screen.height;
@@ -40,11 +27,6 @@ namespace Mobile
                 rectTransform.anchorMin = minAnchor;
                 rectTransform.anchorMax = maxAnchor;
             }
-        }
-
-        private bool EqualRectSize(RectTransform thisRT, Rect safeArea)
-        {
-            return thisRT.rect.height == safeArea.height;
         }
     }
 }
